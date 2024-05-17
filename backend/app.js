5a0b148150c7bd7da07f7e877c1d5111f3bc4c97
Riiturii <riiturii@gmail.com>
2024-05-17 09:19:40 +0200

    Expresse + Nodemon added

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log('requête reçue');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
     res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+});
+
+app.use((req, res) => {
+    console.log('réponse envoyée avec succés');
 });
 
 module.exports = app;
