5a93bf524744565366d54f7b1e543b6530083b05
Riiturii <riiturii@gmail.com>
2024-05-20 17:19:22 +0200

    create token added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
+
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -29,7 +32,11 @@ exports.login = (req, res, next) => {
                         } else {
                             res.status(200).json({
                                 userId: user._id,
-                                token: 'TOKEN'
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    'RANDOM_TOKEN_SECRET',
+                                    { expiresIn: '24h'}
+                                )
                             });
                         }
                     })
