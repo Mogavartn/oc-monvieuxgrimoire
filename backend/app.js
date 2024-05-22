6f598d76e29baa8730d34f4e35fd77c8f045609d
Riiturii <riiturii@gmail.com>
2024-05-22 17:30:59 +0200

    upload modify delete files OK

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
+const app = express();
 
 mongoose
     .connect(
@@ -13,8 +15,9 @@ mongoose
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express();
+app.use(express.json());
 
+//CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -28,8 +31,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json());
-
+app.use("/images", express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
