70b0695d59693d697393a078e0c07417b78a5335
Riiturii <riiturii@gmail.com>
2024-06-27 09:44:26 +0200

    db name removed

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,13 @@ const path = require('path'); // Importation du module Path pour la gestion des 
 
 // Validation des variables d'environnement
 const { DB_USERNAME, DB_PASSWORD, DB_CLUSTER, DB_NAME, FRONTEND_URL } = process.env;
-if (!DB_USERNAME || !DB_PASSWORD || !DB_CLUSTER || !DB_NAME || !FRONTEND_URL) {
+if (!DB_USERNAME || !DB_PASSWORD || !DB_CLUSTER || !FRONTEND_URL) {
     console.error('Erreur: Certaines variables d\'environnement ne sont pas définies');
     process.exit(1);
 }
 
 // Connexion à la base de données MongoDB
-const dbUri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+const dbUri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(dbUri)
     .then(() => console.log('Connexion à MongoDB réussie !')) // Message de succès en cas de connexion réussie
     .catch((error) => console.error('Connexion à MongoDB échouée !', error)); // Message d'erreur en cas de connexion échouée
